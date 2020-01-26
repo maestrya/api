401afe062c00b3034800b3a440d4501d020b58c1
velrino <denismagalhaesvelrino@gmail.com>
2020-01-25 22:40:11 -0300

    🐛  BUG: routes

--- start/routes.js
+++ start/routes.js
@@ -1,18 +1,5 @@
 'use strict'
 
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| Http routes are entry points to your web application. You can create
-| routes for different URLs and bind Controller actions to them.
-|
-| A complete guide on routing is available here.
-| http://adonisjs.com/docs/4.1/routing
-|
-*/
-
 /** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
 const Route = use('Route')
 
@@ -23,5 +10,6 @@ Route.get('/', () => {
 const dir = 'App/Domain/Commands';
 
 Route.group(() => {
-  Route.get('/', `${dir}/Simian/get.execute`)
-}).prefix('api/simian')
+  Route.get('/stats', `${dir}/Simian/get.execute`)
+  Route.post('/simian', `${dir}/Simian/create.execute`)
+}).prefix('api')
