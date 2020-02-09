61be0cfa31c46d24224c94b24cd2e83a27fd5494
velrino <denismagalhaesvelrino@gmail.com>
2020-02-09 09:17:02 -0300

    :art: improving get for found and not found page

--- app/Domain/Commands/Pages/get.js
+++ app/Domain/Commands/Pages/get.js
@@ -4,14 +4,19 @@ const { Enums: { apiResponse } } = use('App/Helpers/enums');
 const { PagesRepository } = use('App/Infrastructure/Repositories/PagesRepository');
 
 class PagesGetCommand extends DefaultCommand {
-  async execute ({ response }) {
+  async execute ({ request, response }) {
     let result = this.resultDefault();
 
     try {
-      const pagesData = await new PagesRepository().first();
+      const pagesData = await new PagesRepository().whereName(request.params.name);
 
-      result.body = pagesData;
-      result.code = apiResponse.codes.success;
+      if (pagesData) {
+        result.body = pagesData;
+        result.code = apiResponse.codes.success;
+      } else if (!pagesData) {
+        result.body = { message: apiResponse.errors.pages.notFound };
+        result.code = apiResponse.codes.notFound;
+      }
     } catch (error) {
       result.body = { message: apiResponse.errors.pages.getUnprocessed };
       result.code = apiResponse.codes.unprocessed;
