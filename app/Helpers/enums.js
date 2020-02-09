8499cb44a7c8dc6147814de07c22f0db0eae654b
velrino <denismagalhaesvelrino@gmail.com>
2020-02-09 09:16:42 -0300

    :zap: improving enums

--- app/Helpers/enums.js
+++ app/Helpers/enums.js
@@ -1,19 +1,25 @@
 const Enums = {
-    apiResponse: {
-        codes: {
-            badRequest: 400,
-            created: 201,
-            forbidden: 403,
-            success: 200,
-            unprocessed: 422
-        },
-        errors : {
-            pages: {
-                forbidden: 'FORBIDDEN',
-                getUnprocessed: 'UNPROCESSED',
-            }
-        }
+  apiResponse: {
+    codes: {
+      badRequest: 400,
+      created: 201,
+      forbidden: 403,
+      notFound: 404,
+      success: 200,
+      unprocessed: 422
+    },
+    errors: {
+      generic: {
+        notFound: 'NOT_FOUND',
+        getUnprocessed: 'UNPROCESSED'
+      },
+      pages: {
+        notFound: 'NOT_FOUND_PAGE',
+        forbidden: 'FORBIDDEN',
+        getUnprocessed: 'UNPROCESSED'
+      }
     }
-}
+  }
+};
 
-module.exports = { Enums }
+module.exports = { Enums };
