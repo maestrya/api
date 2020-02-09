a4a541b325ce43a22d871a43c1208cd9a5ce14e9
velrino <denismagalhaesvelrino@gmail.com>
2020-02-09 08:12:42 -0300

    :zap: remove JSON.stringify because model handle this now

--- database/seeds/DatabaseSeeder.js
+++ database/seeds/DatabaseSeeder.js
@@ -1,98 +1,31 @@
-'use strict'
+'use strict';
 
-const Database = use('Database')
-const DnasModel = use('App/Models/Dnas')
-const DnasItensModel = use('App/Models/DnasItens')
-const Env = use('Env')
+const Database = use('Database');
+const PagesModel = use('App/Models/Pages');
+const Env = use('Env');
+const { PagesData } = use('./Data/Pages');
 
 class DatabaseSeeder {
-  async run() {
-    const foreignKeyOff = (Env.get('DB_CONNECTION') == "mysql") ? 'SET FOREIGN_KEY_CHECKS = 0;' : 'PRAGMA foreign_keys = OFF;';
-    const foreignKeyOn = (Env.get('DB_CONNECTION') == "mysql") ? 'SET FOREIGN_KEY_CHECKS = 1;' : 'PRAGMA foreign_keys = ON;';
+  async run () {
+    const foreignKeyOff =
+			Env.get('DB_CONNECTION') == 'mysql' ? 'SET FOREIGN_KEY_CHECKS = 0;' : 'PRAGMA foreign_keys = OFF;';
+    const foreignKeyOn =
+			Env.get('DB_CONNECTION') == 'mysql' ? 'SET FOREIGN_KEY_CHECKS = 1;' : 'PRAGMA foreign_keys = ON;';
 
     await Database.raw(foreignKeyOff);
 
-    await DnasModel.truncate();
-    await DnasItensModel.truncate();
+    await PagesModel.truncate();
 
-    await DnasModel.createMany([{
-      id: '3fbc4824-1f94-41e1-9c55-b20c407c394c',
-      is_simian: true,
-    },
-    {
-      id: '84ca67e2-71f3-4af2-8525-8b8f12275ca0',
-      is_simian: false,
-    }
+    await PagesModel.createMany([
+      {
+        id: '3fbc4824-1f94-41e1-9c55-b20c407c394c',
+        name: 'page_test',
+        data: PagesData,
+      }
     ]);
 
-
-    await DnasItensModel.createMany([
-      //3fbc4824-1f94-41e1-9c55-b20c407c394c
-      {
-        dna_id: '3fbc4824-1f94-41e1-9c55-b20c407c394c',
-        value: 'CTGAGA',
-        is_simian: true,
-      },
-      {
-        dna_id: '3fbc4824-1f94-41e1-9c55-b20c407c394c',
-        value: 'CTGAGC',
-        is_simian: true,
-      },
-      {
-        dna_id: '3fbc4824-1f94-41e1-9c55-b20c407c394c',
-        value: 'TATTGT',
-        is_simian: true,
-      },
-      {
-        dna_id: '3fbc4824-1f94-41e1-9c55-b20c407c394c',
-        value: 'AGAGGG',
-        is_simian: true,
-      },
-      {
-        dna_id: '3fbc4824-1f94-41e1-9c55-b20c407c394c',
-        value: 'CCCCTA',
-        is_simian: true,
-      },
-      {
-        dna_id: '3fbc4824-1f94-41e1-9c55-b20c407c394c',
-        value: 'TCACTG',
-        is_simian: false,
-      },
-      //84ca67e2-71f3-4af2-8525-8b8f12275ca0
-      {
-        dna_id: '84ca67e2-71f3-4af2-8525-8b8f12275ca0',
-        value: 'CTGAGA',
-        is_simian: false,
-      },
-      {
-        dna_id: '84ca67e2-71f3-4af2-8525-8b8f12275ca0',
-        value: 'CTGAGC',
-        is_simian: false,
-      },
-      {
-        dna_id: '84ca67e2-71f3-4af2-8525-8b8f12275ca0',
-        value: 'TATTGT',
-        is_simian: false,
-      },
-      {
-        dna_id: '84ca67e2-71f3-4af2-8525-8b8f12275ca0',
-        value: 'AGAGGG',
-        is_simian: false,
-      },
-      {
-        dna_id: '84ca67e2-71f3-4af2-8525-8b8f12275ca0',
-        value: 'CCCCTA',
-        is_simian: false,
-      },
-      {
-        dna_id: '84ca67e2-71f3-4af2-8525-8b8f12275ca0',
-        value: 'TCACTG',
-        is_simian: false,
-      },
-    ]);
-
-    await Database.raw(foreignKeyOn)
+    await Database.raw(foreignKeyOn);
   }
 }
 
-module.exports = DatabaseSeeder
+module.exports = DatabaseSeeder;
