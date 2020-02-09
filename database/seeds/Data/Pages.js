cfc5358679d9b4de85a8393d7fc005f0d414fc14
velrino <denismagalhaesvelrino@gmail.com>
2020-02-09 09:13:00 -0300

    :lipstick: changed structure

--- database/seeds/Data/Pages.js
+++ database/seeds/Data/Pages.js
@@ -1,99 +1,97 @@
 const PagesData = {
-  page_test: {
-    header: {
-      title: {
+  header: {
+    title: {
+      accessibility: 'value',
+      text: 'value'
+    }
+  },
+  body: {
+    type: 'static',
+    render: [
+      {
+        type: 'text',
         accessibility: 'value',
-        text: 'value'
+        value: {
+          header: 'Lorem Ipsum',
+          text: 'Lorem Ipsum'
+        }
+      },
+      {
+        type: 'input',
+        value: {
+          name: 'page_test_input_cpf',
+          value: 'value_cpf',
+          mask: 'cpf',
+          type: 'text',
+          validation: ['required'],
+          events: {
+            change: 'page_test.page_test_input_cpf.change'
+          }
+        }
+      },
+      {
+        type: 'input',
+        value: {
+          name: 'page_test_input_pass',
+          value: 'value_password',
+          mask: null,
+          type: 'password',
+          validation: ['required'],
+          events: {
+            change: 'page_test.page_test_input_pass.change'
+          }
+        }
+      },
+      {
+        type: 'select',
+        value: {
+          name: 'page_test_input_select',
+          value: 3,
+          mask: null,
+          type: 'select',
+          validation: ['required'],
+          events: {
+            change: 'page_test.page_test_input_select.change'
+          },
+          options: [
+            {
+              id: 1,
+              text: 'Lorem Ipsum'
+            },
+            {
+              id: 2,
+              text: 'Ipsum lorem'
+            },
+            {
+              id: 3,
+              text: 'Birl'
+            }
+          ]
+        }
       }
-    },
-    body: {
-      type: 'static',
+    ]
+  },
+  footer: {
+    buttons: {
+      type: 'side',
       render: [
         {
-          type: 'text',
-          accessibility: 'value',
-          value: {
-            header: 'Lorem Ipsum',
-            text: 'Lorem Ipsum'
-          }
-        },
-        {
-          type: 'input',
-          value: {
-            name: 'page_test_input_cpf',
-            value: 'value_cpf',
-            mask: 'cpf',
-            type: 'text',
-            validation: ['required'],
-            events: {
-              change: 'page_test.page_test_input_cpf.change'
-            }
-          }
-        },
-        {
-          type: 'input',
-          value: {
-            name: 'page_test_input_pass',
-            value: 'value_password',
-            mask: null,
-            type: 'password',
-            validation: ['required'],
-            events: {
-              change: 'page_test.page_test_input_pass.change'
-            }
-          }
-        },
-        {
-          type: 'select',
-          value: {
-            name: 'page_test_input_select',
-            value: 3,
-            mask: null,
-            type: 'select',
-            validation: ['required'],
-            events: {
-              change: 'page_test.page_test_input_select.change'
-            },
-            options: [
-              {
-                id: 1,
-                text: 'Lorem Ipsum'
-              },
-              {
-                id: 2,
-                text: 'Ipsum lorem'
-              },
-              {
-                id: 3,
-                text: 'Birl'
-              }
-            ]
+          name: 'page_test_submit',
+          accessibility: 'Continuar',
+          color: 'blue',
+          body: {
+            cpf: null,
+            password: null
+          },
+          disabled: true,
+          text: 'Continuar',
+          events: {
+            click: 'page_test.page_test_submit.click'
           }
         }
       ]
-    },
-    footer: {
-      buttons: {
-        type: 'side',
-        render: [
-          {
-            name: 'page_test_submit',
-            accessibility: 'Continuar',
-            color: 'blue',
-            body: {
-              cpf: null,
-              password: null
-            },
-            disabled: true,
-            text: 'Continuar',
-            events: {
-              click: 'page_test.page_test_submit.click'
-            }
-          }
-        ]
-      }
     }
   }
 };
 
-module.exports = { PagesData }
+module.exports = { PagesData };
